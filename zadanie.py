45508744ded3f24368c42b3d9c332ae7b6ddd88b
Dominic <122484201+DewPL@users.noreply.github.com>
2023-02-10 17:29:33 +0100

    Add files via upload

--- zadanie.py
+++ zadanie.py
@@ -1,3 +1,4 @@
+
 import sqlite3
 
 conn = sqlite3.connect('zadanie_DB.sqlite')
@@ -6,58 +7,89 @@ cur = conn.cursor()
 # Tworzenie DB
 cur.execute('DROP TABLE IF EXISTS Zadanie') # Usunie DB jeśli istnieje
 cur.execute('''CREATE TABLE Zadanie
-                (kategoria TEXT, nazwa TEXT, cena FLOAT, data TEXT)''')
+                (category TEXT, name TEXT, amount FLOAT, date TEXT);''')
 
 # Wstawianie danych
 cur.execute("""INSERT INTO Zadanie VALUES
-                ('Samochody',
-                'BMW 325',
-                50000,
-                '2005')""")
+            ('1',
+            'apples',
+            2.40,
+            '2021-02-03');""")
 
 cur.execute("""INSERT INTO Zadanie VALUES
-                ('Samochody',
-                'Mercedes SLK',
-                60000,
-                '2005')""")
-            
-cur.execute("""INSERT INTO Zadanie VALUES
-                ('Samochody',
-                'Fiat 126p',
-                200,
-                '2000')""")
+            ('1',
+            'bread',
+            2.00,
+            '2021-02-03');""")
 
 cur.execute("""INSERT INTO Zadanie VALUES
-                ('Samochody',
-                'Peugeot 3008',
-                3500,
-                '2015')""")
+            ('1',
+            'salami',
+            4.30,
+            '2021-02-02');""")
 
 cur.execute("""INSERT INTO Zadanie VALUES
-                ('Motocykle',
-                'Kawasaki EL250',
-                6500,
-                '2000')""")
+            ('1',
+            'butter',
+            5.20,
+            '2021-02-02');""")
 
 cur.execute("""INSERT INTO Zadanie VALUES
-                ('Motocykle',
-                'Ducati 1199 Panigale',
-                65000,
-                '2012')""")
+            ('1',
+            'butter',
+            5.20,
+            '2020-12-27');""")
 
 cur.execute("""INSERT INTO Zadanie VALUES
-                ('Motocykle',
-                'Harley Davidson Fat Boy 114',
-                '------',
-                '2023')""")
+            ('2',
+            'rent',
+            000.00,
+            '2021-02-04');""")
+
+cur.execute("""INSERT INTO Zadanie VALUES
+            ('2',
+            'rent',
+            000.00,
+            '2020-12-27');""")
+
+cur.execute("""INSERT INTO Zadanie VALUES
+            ('2',
+            'electricity',
+            42.00,
+            '2021-02-04');""")
+
+cur.execute("""INSERT INTO Zadanie VALUES
+            ('2',
+            'electricity',
+            42.00,
+            '2020-12-07');""")
+
+cur.execute("""INSERT INTO Zadanie VALUES
+            ('3',
+            'trousers',
+            110.00,
+            '2021-02-04');""")
+
+cur.execute("""INSERT INTO Zadanie VALUES
+            ('3',
+            'shirt',
+            60.00,
+            '2021-01-16');""")
+
+cur.execute("""INSERT INTO Zadanie VALUES
+            ('3',
+            'shirt',
+            60.00,
+            '2020-12-25');""")
 
 # zapisanie zmian
 conn.commit()
 
-# Odczyt danych z SQLite
+# sortowanie danych w bazie poprzez zapytania SQL i 
+# Odczyt danych z bazy
 print('Odczytanie danych Zapytanie sql select *')
-print('Baza danych | Marka model  |  cena  | rok')
-cur.execute("SELECT * FROM Zadanie")
+print('| kat. |  nazwa  | cena | data |')
+cur.execute("SELECT * FROM Zadanie;")
 
 for row in cur:
     print(row)
@@ -65,8 +97,8 @@ print()
 
 # odczyt danych według daty ASC
 print('Odczytanie danych według daty rosnąco')
-print('Baza danych | Marka model  |  cena  | rok')
-cur.execute("SELECT * FROM Zadanie ORDER BY data ASC")
+print('| kat. |  nazwa  | cena | data |')
+cur.execute("SELECT * FROM Zadanie ORDER BY date ASC;")
 
 for row in cur:
     print(row)
@@ -75,18 +107,8 @@ print()
 
 # odczyt danych według daty DESC
 print('Odczytanie danych według daty malejąco')
-print('Baza danych | Marka model  |  cena  | rok')
-cur.execute("SELECT * FROM Zadanie ORDER BY data DESC")
-
-for row in cur:
-    print(row)
-
-print()
-
-# odczyt danych po kategoriach
-print('Odczytanie danych po kategorii')
-print('Baza danych | Marka model  |  cena  | rok')
-cur.execute("SELECT * FROM Zadanie WHERE kategoria = 'Motocykle'")
+print('| kat. |  nazwa  | cena | data |')
+cur.execute("SELECT * FROM Zadanie ORDER BY date DESC;")
 
 for row in cur:
     print(row)
@@ -96,7 +118,32 @@ print()
 #  suma wszystkiego w DB
 print('Sumuje wszystko w bazie po cenie')
 print('SUMA:')
-cur.execute("SELECT SUM(cena) FROM Zadanie")
+cur.execute("SELECT SUM(amount) FROM Zadanie;")
+
+for row in cur:
+    print(row)
+
+print()
+
+# odczyt danych po kategoriach
+print('Odczytanie danych po kategorii')
+kat = input("Podaj kategorię: ")
+print('| kat. |  nazwa  | cena | data |')
+cur.execute("SELECT * FROM Zadanie WHERE category = " + kat)
+
+for row in cur:
+    print(row)
+
+print()
+
+# Suma dla wybranej daty
+# q SQL użyć można DATEPART i funkcji GETDATE()
+print('Odczytanie danych według daty podanej przez użytkownika')
+data_IN = input("Podaj datę: ")
+print('SUMA')
+cur.execute("SELECT SUM(amount) FROM Zadanie WHERE date >= " + data_IN)
+# cur = self.conn.cursor()
+# cur.execute("SELECT datetime('now', 'localtime')")
 
 for row in cur:
     print(row)
@@ -105,26 +152,15 @@ print()
 
 # odczyt danych według daty podanej przez użytkownika
 print('Odczytanie danych według daty podanej przez użytkownika')
-print('Baza danych | Marka model  |  cena  | rok')
-data_IN = input("Podaj rok produkcji: ")
-cur.execute("SELECT * FROM Zadanie WHERE data =" + data_IN)
+data_IN = input("Podaj od: ")
+data_OUT = input("Podaj do: ")
+print('| kat. |  nazwa  | cena | data |')
+cur.execute("SELECT * FROM Zadanie WHERE date >= " + data_IN + " AND date <= " + data_OUT)
 
 for row in cur:
     print(row)
 
 print()
-
-# Suma dla wybranej daty
-print('Odczytanie danych według daty podanej przez użytkownika')
-print('Baza danych | Marka model  |  cena  | rok')
-data_IN = input("Podaj rok produkcji: ")
-cur.execute("SELECT SUM(cena) FROM Zadanie WHERE data =" + data_IN)
-
-for row in cur:
-    print(row)
-
-print()
-
 
 # zamknięcie połączenia
 conn.close()
